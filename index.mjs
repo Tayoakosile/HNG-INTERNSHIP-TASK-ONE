49f121b3eeb9d5d359e9f0fbc781efbce7b11def
Tayo Akosile <oluwatayoakosile@gmail.com>
2023-09-07 20:06:36 +0100

    feat:added updates

--- index.mjs
+++ index.mjs
@@ -1,17 +1,16 @@
 import http from "http";
 
-const port = 4081;
+const port = process.env?.PORT || 4081;
 
 const hostname = "127.0.0.1";
 
 const server = http.createServer((req, res) => {
-  const data = { message: "Hello World" };
+  res.setHeader("Content-Type", "application/json");
+  res.setHeader("Connection", "close");
+  res.statusCode = 200;
+
   if (req.url?.includes("/api")) {
     const url = new URL(req.url, `http://${req.headers.host}`);
-
-    res.setHeader("Content-Type", "application/json");
-    res.setHeader("Connection", "close");
-    res.statusCode = 200;
 
     const date = new Date();
     const daysOfTheWeek = [
@@ -23,13 +22,12 @@ const server = http.createServer((req, res) => {
       "Friday",
       "Saturday",
     ];
-    console.log(date.toJSON().split('.'));
 
     const data = {
       slack_name: url.searchParams?.get("slack_name"),
       current_day: daysOfTheWeek[date.getDay()],
-      // utc_time: "2023-08-21T15:04:05Z",
-      utc_time: date.toJSON().split('.')[0] + 'Z',
+
+      utc_time: date.toJSON().split(".")[0] + "Z",
       track: url.searchParams?.get("track"),
       github_file_url:
         "https://github.com/Tayoakosile/HNG-INTERNSHIP-TASK-ONE/blob/master/index.mjs",
@@ -37,9 +35,8 @@ const server = http.createServer((req, res) => {
     };
     return res.end(JSON.stringify(data));
   }
-  res.end(JSON.stringify(data));
-
-  // res.end(JSON.stringify(req.url));
+  const data = { message: "Hello World" };
+  return res.end(JSON.stringify(data));
 });
 
 server.listen(port, hostname, () => {
